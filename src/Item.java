1f4258fad1e8bae341d1b38c896d2197d5149081
L1am0connor <2266952@swansea.ac.uk>
2023-06-10 13:11:09 +0100

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,8 +1,23 @@
 /**
  * This class describes a collectible item rewarded after every battle.
- * @author jackroberts
+ * @author Liam O'Connor
  *
  */
-public class Item {
 
+public class Item {
+    private String itemType;
+    private int goldCost;
+    
+    public Item(String itemType, int goldCost) {
+        this.itemType = itemType;
+        this.goldCost = goldCost;
+    }
+    
+    public String getItemType() {
+        return itemType;
+    }
+    
+    public int getGoldCost() {
+        return goldCost;
+    }
 }
