32b491545eaf07c770292b916a4d101c73353015
A-R05SI <1907231@swansea.ac.uk>
2023-06-17 10:25:00 +0100

    Updated the code for Battle Manager

--- src/BattleManager.java
+++ src/BattleManager.java
@@ -1,8 +1,44 @@
+import java.util.Random;
 /**
  * A class decribing BattleManager - a class to handle Battle gameplay.
  * @author jackroberts
  *
  */
 public class BattleManager {
+    public BattleManager(Actor Player, Actor Monster){
+        boolean battle_over = false;
+        String turn = "player";
+        while(!battle_over) {
+            if (turn.equals("player")){
+                playerAttack(Player, Monster);
+            } else {
+                monsterAttack(Player, Monster);
+            }
+        }
+    }
 
+    public static void main(Actor Player, Actor Monster) {
+        boolean battle_over = false;
+        String turn = "player";
+        while(!battle_over) {
+            if (turn.equals("player")){
+                playerAttack(Player, Monster);
+            } else {
+                monsterAttack(Player, Monster);
+            }
+        }
+    }
+
+
+    private static void playerAttack(Actor Player, Actor Monster){
+
+    }
+
+    private static void monsterAttack(Actor Player, Actor Monster){
+        final int MINIMUM_ATTACK = 0;
+        final int MAXIMUM_ATTACK = 20;
+        Random random = new Random();
+        
+        System.out.println(random.nextInt(MAXIMUM_ATTACK));
+    }
 }
