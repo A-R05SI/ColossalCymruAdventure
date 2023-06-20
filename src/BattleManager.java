4c040a97d0e20373aaae5cc60bfd3cc75b69fd3e
A-R05SI <1907231@swansea.ac.uk>
2023-06-20 21:34:07 +0100

    The rest of the logic has been added alongside the winning values that the user will gain when victorious

--- src/BattleManager.java
+++ src/BattleManager.java
@@ -19,7 +19,8 @@ public class BattleManager {
     }
 
     private void startBattle(){
-
+        final int PLAYER_XP_IF_WIN = 100;
+        final int PLAYER_GOLD_IF_WIN = 5;
         while(!is_battle_over()) {
             isPlayerTurn = !isPlayerTurn; // In first run, allows for player to go first
             if (isPlayerTurn){
@@ -29,26 +30,51 @@ public class BattleManager {
             }
         }
         if (isPlayerTurn){
-            System.out.println(this.player.getName() + " has died.");
+            System.out.println(player.getName() + " has died.");
             System.out.println("Game Over.");
             System.exit(0);
+        } else {
+            System.out.printf("%s has been killed.", monster.getName());
+            System.out.printf("%s has won the battle.", player.getName());
+
+            player.addExperience(PLAYER_XP_IF_WIN);
+            player.addGold(PLAYER_GOLD_IF_WIN);
+            player.addToInventory(monster.loot);
         }
     }
 
     private void playerAttack(){
-        
+        int attack = player.getCurrentWeapon().getDamage();
+        int ac = monster.getArmour();
+        if(attack == 0){
+            System.out.printf("%s missed.", player.getName());
+        } else if (attack <= ac){
+            System.out.printf("%s struck a glancing blow!", player.getName());
+        } else {
+            int damageDealt = attack - ac;
+            monster.setHealth(monster.getHealth() - damageDealt);
+            System.out.printf("%s landed a hit for %s damage!", player.getName(), damageDealt);
+        }
     }
 
     private void monsterAttack(){
         final int MAXIMUM_ATTACK = 20;
         Random random = new Random();
         int attack = random.nextInt(MAXIMUM_ATTACK + 1); //allows for attacks == MAXIMUM_ATTACK
-        float ac = this.player.getArmour();
-        
+        int ac = player.getArmour();
+        if(attack == 0){
+            System.out.printf("%s missed.", monster.getName());
+        } else if (attack <= ac){
+            System.out.printf("%s struck a glancing blow!", monster.getName());
+        } else {
+            int damageDealt = attack - ac;
+            player.setHealth(player.getHealth() - damageDealt);
+            System.out.printf("%s landed a hit for %s damage!", monster.getName(), damageDealt);
+        }
     }
 
     private boolean is_battle_over(){
-        if((this.player.getHealth() > 0) && (this.monster.getHealth() > 0)){
+        if((player.getHealth() > 0) && (monster.getHealth() > 0)){
             return false;
         } else {
             return true;
